54dc3583bc956f15154e2e65bbbfe1bcc8a06dec
mhmd <mohamad_kaiss@hotmail.com>
2021-03-26 01:57:11 +0100

    kha

--- pages/Documentation.js
+++ pages/Documentation.js
@@ -1,132 +1,3 @@
-// import React from 'react';
-// import {View, StyleSheet,Image, Button, Platform} from 'react-native';
-// import database from '@react-native-firebase/database';
-// import Header from '../components/Header';
-// import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
-// import storage from '@react-native-firebase/storage';
-
-
-// class DocumentationPage extends React.Component {  
-
-//   state = {image:null,uploading:false,transferred:0};
-
-//   selectImage(){
-//     // const options = {
-//     //   maxWidth: 2000,
-//     //   maxHeight: 2000,
-//     //   storageOptions: {
-//     //     skipBackup: true,
-//     //     path: 'images'
-//     //   }
-//     // };
-//     // ImagePicker.showImagePicker(options, response => {
-//     //   if (response.didCancel) {
-//     //     console.log('User cancelled image picker');
-//     //   } else if (response.error) {
-//     //     console.log('ImagePicker Error: ', response.error);
-//     //   } else if (response.customButton) {
-//     //     console.log('User tapped custom button: ', response.customButton);
-//     //   } else {
-//     //     const source = { uri: response.uri };
-//     //     console.log(source);
-//     //     setImage(source);
-//     //   }
-//     // });
-//   };
-
-//   // async chooseFile() {
-//   //     // try {
-//   //     //   const file = await DocumentPicker.pick({
-//   //     //     type: [DocumentPicker.types.allFiles],
-//   //     //   });
-//   //     //   // const path = await normalizePath(file.uri);
-//   //     //   // console.log(path);
-//   //     //   // console.log(
-//   //     //   //   file.uri,
-//   //     //   //   file.type, // mime type
-//   //     //   //   file.name,
-//   //     //   //   file.size
-//   //     //   // );
-//   //     // } catch (err) {
-//   //     //   if (DocumentPicker.isCancel(err)) {
-//   //     //     // User cancelled the picker, exit any dialogs or menus and move on
-//   //     //   } else {
-//   //     //     throw err;
-//   //     //   }
-//   //     // }
-//   // }
-
-//   // async normalizePath(path) {
-//   //   if(Platform.OS==='os' || Platform.OS==='android'){
-//   //     const filePrefix='file://';
-//   //     if(path.startsWith(filePrefix)){
-//   //       path= path.substring(filePrefix.length);
-//   //       try{
-//   //         path = decodeURI(path);
-//   //       }catch(e){
-
-//   //       }
-//   //     }
-//   //   }
-//   //   return path;
-//   // }
-  
- 
-//   render(){
-
-//     // const choosePhotoFromLibrary = () => {
-//     //     ImagePicker
-//     // }
-
-
-//     return (
-        
-//         <View style={styles.containerForm}>
-
-//           <Header headerText={'Documentation'}/>
-
-//           <View style={styles.imageContainer}>
-//               <Image style={styles.imageStyle} source={require('../assets/Nord-Quest.png')}/>
-//           </View>
-          
-//           <View style={styles.PublicitesStyleContainer}>
-//             <Button title={"choose pic"} onPress={this.selectImage.bind(this)}/>
-             
-          
-//           </View>
-        
-        
-//         </View>
-           
-//     );
-//   };
-// }
-
-// const styles= StyleSheet.create({
-//   containerForm:{
-//     flex:1,
-//     backgroundColor:'white'
-//   }
-//   ,
-//   imageStyle:{
-//     alignSelf:'flex-end',
-//     height:150,
-//     width:100,
-//   }
-  
-//   ,
-//   PublicitesStyleContainer:{
-//     flex:1,
-//     justifyContent:'center',
-//     alignItems:'center',
-//   }
-//   ,
-// })
-
-
-// export default DocumentationPage;
-
-
 // #3 Uploading Files and Images to Firebase Cloud Storage in React Native
 // https://aboutreact.com/react-native-firebase-cloud-storage/
 
@@ -199,20 +70,8 @@ const DocumentationPage = () => {
       // task.resume();
       // task.cancel();
 
-      task.on("state_changed", (taskSnapshot) => {
-        setProcess(
-          `${taskSnapshot.bytesTransferred} transferred 
-           out of ${taskSnapshot.totalBytes}`
-        );
-        console.log(
-          `${taskSnapshot.bytesTransferred} transferred 
-           out of ${taskSnapshot.totalBytes}`
-        );
-      });
-      task.then(() => {
-        alert("Image uploaded to the bucket!");
-        setProcess("");
-      });
+     
+     
       setFilePath({});
     } catch (error) {
       console.log("Error->", error);
@@ -233,16 +92,16 @@ const DocumentationPage = () => {
     var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
     console.log('Upload is ' + progress + '% done');
     switch (snapshot.state) {
-      case firebase.storage.TaskState.PAUSED: // or 'paused'
+      case storage.TaskState.PAUSED: // or 'paused'
         console.log('Upload is paused');
         break;
-      case firebase.storage.TaskState.RUNNING: // or 'running'
+      case  storage.TaskState.RUNNING: // or 'running'
         console.log('Upload is running');
         break;
     }
   }, 
   (error) => {
-    // Handle unsuccessful uploads
+      console.log(error);
   }, 
   () => {
     // Handle successful uploads on complete
